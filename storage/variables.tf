4f1e91b73f157408d56347948985f55883f8092a
Janluke <gianlucacaronte@gmail.com>
2021-10-17 14:17:31 +0200

    on-created trigger in storage module

--- storage/variables.tf
+++ storage/variables.tf
@@ -1,2 +1,5 @@
 variable "region" {}
 variable "user_pool_domain" {}
+variable "on_created_lambda" { default = null }
+variable "on_removed_lambda" { default = null }
+#variable "on_download_lambda" {}
