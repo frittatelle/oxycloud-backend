0c76c2eebf8d739fde9c7768a5e2fdcc5c957d87
Janluke <gianlucacaronte@gmail.com>
2021-10-21 21:52:51 +0200

    remove commented and unused code

--- storage/triggers.tf
+++ storage/triggers.tf
@@ -1,16 +1,8 @@
 resource "aws_lambda_permission" "allow_bucket_on_created" {
-  #  count         = var.on_created_lambda_arn == null ? 0 : 1
+  count = var.on_created_lambda_arn == null ? 0 : 1
   statement_id  = "AllowExecutionFromS3BucketOnCreated"
   action        = "lambda:InvokeFunction"
   function_name = var.on_created_lambda_arn
-  principal     = "s3.amazonaws.com"
-  source_arn    = aws_s3_bucket.storage.arn
-}
-resource "aws_lambda_permission" "allow_bucket_on_removed" {
-  count = 0#         = var.on_removed_lambda_arn == null ? 0 : 1
-  statement_id  = "AllowExecutionFromS3BucketOnRemoved"
-  action        = "lambda:InvokeFunction"
-  function_name = var.on_removed_lambda_arn
   principal     = "s3.amazonaws.com"
   source_arn    = aws_s3_bucket.storage.arn
 }
@@ -18,27 +10,15 @@ resource "aws_lambda_permission" "allow_bucket_on_removed" {
 #BIG ISSUE
 #we are allowed to do only one resource per bucket
 #but i cannot put condition for lambda block
+#at this time we just trigger creation
 resource "aws_s3_bucket_notification" "triggers" {
   bucket = aws_s3_bucket.storage.id
 
-  count = 1#var.on_created_lambda_arn == null ? 0 : 1
+  count = var.on_created_lambda_arn == null ? 0 : 1
 
   lambda_function {
     lambda_function_arn = var.on_created_lambda_arn
     events              = ["s3:ObjectCreated:*"]
   }
 
-  #dynamic "lambda_function" {
-  #  for_each = [var.on_removed_lambda]
-  #  content {
-  #    lambda_function_arn = lambda_function.value["arn"]
-  #    events              = ["s3:ObjectRemoved:*"]
-  #  }
-  #}
-
-  # There isn't an event for this btw we can use cloudwatch
-  #  lambda_function {
-  #    lambda_function_arn = "${on_download_lambda.arn}"
-  #    events              = ["s3:ObjectCreated:*"]
-  #  }
 }
--- storage/variables.tf
+++ storage/variables.tf
@@ -1,5 +1,3 @@
 variable "region" {}
 variable "user_pool_domain" {}
 variable "on_created_lambda_arn" { default = null }
-variable "on_removed_lambda_arn" { default = null }
-#variable "on_download_lambda" {}
