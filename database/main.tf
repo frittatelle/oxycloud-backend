b702ca3f0f91c433a6baabccc13205229ce8cbe1
Janluke <gianlucacaronte@gmail.com>
2021-12-03 15:29:45 +0100

    add database module

---
+++ database/main.tf
@@ -0,0 +1,24 @@
+module "dynamodb_table" {
+  source   = "terraform-aws-modules/dynamodb-table/aws"
+
+  name     = "users_storage_files"
+  hash_key = "file_id"
+  range_key = "user_id"
+  attributes = [
+    {
+      name = "file_id"
+      type = "S"
+    },
+    {
+      name = "user_id"
+      type = "S"
+    },
+ ]
+}
+
+output "table" {
+  value = tomap({
+    arn = "${module.dynamodb_table.dynamodb_table_arn}",
+    name = "${module.dynamodb_table.dynamodb_table_id}" ,
+  })
+}
