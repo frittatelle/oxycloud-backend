7c35c5076a7ae3a61a2fba073308127bf8752d81
frittatelle <myfavouritefreakelle@gmail.com>
2021-12-10 19:28:43 +0100

    deleteFile fixed - hardcoded region, hardcoded table name, mapping integration fix

--- API/deleteFile.tf
+++ API/deleteFile.tf
@@ -15,7 +15,7 @@ resource "aws_api_gateway_integration" "DeleteDoc" {
   rest_api_id             = aws_api_gateway_rest_api.OxyApi.id
   resource_id             = aws_api_gateway_resource.DocID.id
   http_method             = aws_api_gateway_method.DeleteDoc.http_method
-  integration_http_method = aws_api_gateway_method.DeleteDoc.http_method
+  integration_http_method = "POST"
   content_handling        = "CONVERT_TO_TEXT"
   passthrough_behavior    = "WHEN_NO_TEMPLATES"
   type                    = "AWS"
@@ -28,25 +28,26 @@ resource "aws_api_gateway_integration" "DeleteDoc" {
   uri         = "arn:aws:apigateway:${var.region}:dynamodb:action/UpdateItem"
   request_templates = {
     "application/json" = <<EOF
-    #set($user_id = $context.authorizer.claims['cognito:username'])
-    {
-      "TableName":"${var.storage_table.name}",
-      "Key":{
-          "file_id":{
-              "S":"$method.request.path.id"
-          },
-          "user_id":{
-            "S":"$user_id"
-          }
-      },
-      "UpdateExpression": "set deleted = :deleted",
-      "ConditionExpression": "file_id = :file_id AND user_id = :user_id",
-      "ExpressionAttributeValues": {
-          ":deleted": {"BOOL": "$method.request.querystring.deleted"},
-          ":file_id": {"S: "$method.request.path.file_id"},
-          ":user_id": {"S: "$user_id"}
-      },
-    }
+  #set($user_id = $context.authorizer.claims['cognito:username'])
+  {
+    "TableName":"${var.storage_table.name}",
+    "Key":{
+        "file_id":{
+            "S":"$method.request.path.id"
+        },
+        "user_id":{
+          "S":"$user_id"
+        }
+    },
+    "UpdateExpression": "set deleted = :deleted",
+    "ExpressionAttributeValues": {
+        ":deleted": {"BOOL": $method.request.querystring.deleted},
+        ":file_id": {"S": "$method.request.path.id"},
+        ":user_id": {"S": "$user_id"}
+    },
+    "ConditionExpression": "file_id = :file_id AND user_id = :user_id",
+    "ReturnValues": "ALL_NEW"
+  }
     EOF
   }
 }
