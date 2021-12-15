31b8ee4ae4d097fa9b55b8473faaae783ce1288b
Janluke <gianlucacaronte@gmail.com>
2021-12-15 12:08:07 +0100

    fix invokation error on PUT /docs

--- API/uploadFile/method.tf
+++ API/uploadFile/method.tf
@@ -15,7 +15,8 @@ resource "aws_api_gateway_integration" "UploadDoc" {
   rest_api_id             = var.rest_api_id
   resource_id             = var.resource_id
   http_method             = aws_api_gateway_method.UploadDoc.http_method
-  integration_http_method = aws_api_gateway_method.UploadDoc.http_method
+  #lambda invokation requires a post
+  integration_http_method = "POST"
   type                    = "AWS_PROXY"
   timeout_milliseconds    = 29000
   passthrough_behavior    = "WHEN_NO_MATCH"
